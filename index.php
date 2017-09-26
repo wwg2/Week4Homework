3001f2c203010d4a838e481d94fe634d2f09b72c
Bryan Nissen <bryannissen@gmail.com>
2017-09-26 08:07:05 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	$date =  date('Y-m-d', time());
+	echo "The value of \$date: ".$date."<br>";
+	$tar = "2017/05/24";
+	echo "The value of \$tar: ".$tar."<br>";
+	$year = array("2012", "396", "300","2000", "1100", "1089");
+	echo "The value of \$year: ";
+	print_r($year);
+?>
