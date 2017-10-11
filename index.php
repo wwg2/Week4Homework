dbff047d1e96dc5e5a9dc3235b25ac9aab9551ba
Weston Gallant <wesgallant97@gmail.com>
2017-10-10 22:51:34 -0400

    Problem 1

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-	$date =  date('Y-m-d', time());
+	$date =  date('Y/m/d', time());
 	echo "The value of \$date: ".$date."<br>";
 	$tar = "2017/05/24";
 	echo "The value of \$tar: ".$tar."<br>";
 	$year = array("2012", "396", "300","2000", "1100", "1089");
 	echo "The value of \$year: ";
 	print_r($year);
+	print_r($date);
 ?>
